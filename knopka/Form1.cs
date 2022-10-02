ab9574e840e41f96fcfde2235d9a2b4a1e22ea5e
Кирилл Бабошко <hokagemraz04@mail.ru>
2022-10-02 20:46:54 +0300

    Добавьте файлы проекта.

---
+++ knopka/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace knopka
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Ubegat(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            button2.Left = r.Next(0, this.Size.Width - button2.Width); 
+            button2.Top = r.Next(0, this.Size.Height - button2.Height);
+        }
+    }
+}
